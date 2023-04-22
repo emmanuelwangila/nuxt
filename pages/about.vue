9bee4b1c6c1e037fd46ff9a4a1bd8b7331e1c837
Emmanuel Wangila <79250258+emmanuelwangila@users.noreply.github.com>
2023-04-22 12:21:29 +0300

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged.
+    </p>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 34px;
+}
+p {
+  margin: 20 0;
+}
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="grid grid-cols-4 gap-5">
+      <div></div>
+      <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "products",
+});
+//fetch products
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+</script>
+
+<style lang="scss" scoped></style>
